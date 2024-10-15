72c05c80ece18a2ec4010a4385aa41d76383d99d
Hicham Guessab <hichamgsb.pro@gmail.com>
2024-10-15 17:15:22 +0200

    Compiling MLP with : - categorical_crossentropy function - SGD optimizer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.datasets import mnist
 
 # Exercise 1
 # Construire un MLP avec 2 couches cachées avec 200 neurones chacune et fonction d’activation de type ‘relu’ et une couche de sortie de type ‘softmax’.
@@ -19,3 +20,8 @@ model.add(Dense(10, activation='softmax'))
 # Display model structure
 model.summary()
 
+# Exercise 2
+# Compiler le MLP en y associant une fonction de coût de type cross entropie catégorielle et un optimiseur de type descente stochastique du gradient (SDG).
+# Il sera probablement nécessaire d’importer des packages de Keras.
+
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=["accuracy"])
